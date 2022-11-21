8f625c37cee36d1cf063530f8f0cd650f409e83c
mihai <mihaipoto@gmail.com>
2022-11-21 09:48:12 +0200

    adaugat aplicatie MAUI

---
+++ Aplicatie/Aplicatie/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Aplicatie;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
